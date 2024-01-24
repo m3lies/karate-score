31fce55a799fda183c6176c5dc663813f3562384
m3lies <melanie.manivanh@gmail.com>
2024-01-24 02:59:10 +0100

    added total scores

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {PenaltiesComponent} from "../penalties/penalties.component";
 
 @Component({
   selector: 'app-score',
@@ -6,82 +7,64 @@ import {Component} from '@angular/core';
   styleUrls: ['./score.component.scss'],
 })
 export class ScoreComponent {
-  participant1Score = 0;
-  participant2Score = 0;
-  participant1YukoCount = 0;
-  participant2YukoCount = 0;
-  participant1WazariCount = 0;
-  participant2WazariCount = 0;
-  participant1IpponCount = 0;
-  participant2IpponCount = 0;
-  //Senshu
-  participant1SenshuCount = 0;
-  participant2SenshuCount = 0;
+  @ViewChild(PenaltiesComponent) penaltiesComponent1!: PenaltiesComponent;
+  @ViewChild(PenaltiesComponent) penaltiesComponent2!: PenaltiesComponent;
 
+  totalScores: number[] = [0, 0];
   scores: { yuko: number, wazaAri: number, ippon: number }[] = [
     {yuko: 0, wazaAri: 0, ippon: 0}, // Participant 1 scores
     {yuko: 0, wazaAri: 0, ippon: 0}, // Participant 2 scores
   ];
 
-  generateArray(length: number): number[] {
-    return Array.from({length}, (_, index) => index);
-  }
 
   // Inside your component
   addScore(scoreType: string, participantNumber: number) {
     if (scoreType === 'yuko') {
       this.scores[participantNumber - 1].yuko++;
+      this.calculateTotalScores(participantNumber);
     } else if (scoreType === 'wazaAri') {
       this.scores[participantNumber - 1].wazaAri++;
+      this.calculateTotalScores(participantNumber);
     } else if (scoreType === 'ippon') {
       this.scores[participantNumber - 1].ippon++;
+      this.calculateTotalScores(participantNumber);
     }
   }
 
   removeScore(scoreType: string, participantNumber: number) {
     if (scoreType === 'yuko' && this.scores[participantNumber - 1].yuko > 0) {
       this.scores[participantNumber - 1].yuko--;
+      this.calculateTotalScores(participantNumber);
     } else if (scoreType === 'wazaAri' && this.scores[participantNumber - 1].wazaAri > 0) {
       this.scores[participantNumber - 1].wazaAri--;
+      this.calculateTotalScores(participantNumber);
     } else if (scoreType === 'ippon' && this.scores[participantNumber - 1].ippon > 0) {
       this.scores[participantNumber - 1].ippon--;
+      this.calculateTotalScores(participantNumber);
     }
   }
-
-
-  addSenshu(participant: number) {
-    // Add Senshu (1 point) and increment Senshu count by 1
-    this.participant1SenshuCount += participant === 1 ? 1 : 0;
-    this.participant2SenshuCount += participant === 2 ? 1 : 0;
+  calculateTotalScores(participantNumber: number) {
+    const scores = this.scores[participantNumber - 1];
+    const totalScore = scores.yuko * 1 + scores.wazaAri * 2 + scores.ippon * 3;
+    this.totalScores[participantNumber - 1] = totalScore;
   }
+  resetScore() {
+    // Reset the score properties to their initial values
+    this.scores = [
+      { yuko: 0, wazaAri: 0, ippon: 0 }, // Participant 1 scores
+      { yuko: 0, wazaAri: 0, ippon: 0 }, // Participant 2 scores
+    ];
 
-  incrementYukoCount(participant: number) {
-    // Increment Yuko count
-    if (participant === 1) {
-      this.participant1YukoCount++;
-    } else if (participant === 2) {
-      this.participant2YukoCount++;
-    }
+    // Calculate and reset total scores
+    this.calculateTotalScores(1);
+    this.calculateTotalScores(2);
   }
+  resetScoreAndPenalties(){
+    this.penaltiesComponent1.resetPenalties();
+    this.penaltiesComponent2.resetPenalties();
+    this.resetScore();
 
-  incrementWazariCount(participant: number) {
-    // Increment Waza-ari count
-    if (participant === 1) {
-      this.participant1WazariCount++;
-    } else if (participant === 2) {
-      this.participant2WazariCount++;
-    }
   }
-
-  incrementIpponCount(participant: number) {
-    // Increment Ippon count
-    if (participant === 1) {
-      this.participant1IpponCount++;
-    } else if (participant === 2) {
-      this.participant2IpponCount++;
-    }
-  }
-
 
 }
 
