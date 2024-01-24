b90801819bcba736e9aaac62ef077bcdf6eb9438
m3lies <melanie.manivanh@gmail.com>
2024-01-24 02:07:21 +0100

    changed penalties design

--- src/app/penalties/penalties.component.ts
+++ src/app/penalties/penalties.component.ts
@@ -7,114 +7,39 @@ import {Component, Input} from '@angular/core';
 })
 export class PenaltiesComponent {
   constructor() {}
-  @Input() participantNumber: number =1;
-  participant1Penalties = 0;
-  participant2Penalties = 0;
+  isChui1Clicked: boolean = false;
+  isChui2Clicked: boolean = false;
+  isChui3Clicked: boolean = false;
+  isHansokuChuiClicked: boolean = false;
+  isHansokuClicked: boolean = false;
 
-  // Flags to track if penalties have been applied
-  participant1Chui1Applied = false;
-  participant1Chui2Applied = false;
-  participant1Chui3Applied = false;
-  participant1HansokuChuiApplied = false;
-
-  participant2Chui1Applied = false;
-  participant2Chui2Applied = false;
-  participant2Chui3Applied = false;
-  participant2HansokuChuiApplied = false;
-
-  // ... (previous methods)
-
-  // Methods to check if penalties have been applied
-  isChui1Disabled(participant: number): boolean {
-    return (
-      (participant === 1 && this.participant1Chui1Applied) ||
-      (participant === 2 && this.participant2Chui1Applied)
-    );
+  resetPenalties() {
+    this.isChui1Clicked = false;
+    this.isChui2Clicked = false;
+    this.isChui3Clicked = false;
+    this.isHansokuChuiClicked = false;
+    this.isHansokuClicked = false;
   }
 
-  isChui2Disabled(participant: number): boolean {
-    return (
-      (participant === 1 && this.participant1Chui2Applied) ||
-      (participant === 2 && this.participant2Chui2Applied)
-    );
+
+  toggleChui1() {
+    this.isChui1Clicked = !this.isChui1Clicked;
   }
 
-  isChui3Disabled(participant: number): boolean {
-    return (
-      (participant === 1 && this.participant1Chui3Applied) ||
-      (participant === 2 && this.participant2Chui3Applied)
-    );
+  toggleChui2() {
+    this.isChui2Clicked = !this.isChui2Clicked;
   }
 
-  isHansokuChuiDisabled(participant: number): boolean {
-    return (
-      (participant === 1 && this.participant1HansokuChuiApplied) ||
-      (participant === 2 && this.participant2HansokuChuiApplied)
-    );
+  toggleChui3() {
+    this.isChui3Clicked = !this.isChui3Clicked;
   }
 
-  // Methods to apply penalties
-  addChui1(participant: number) {
-    this.applyChui1(participant);
+  toggleHansokuChui() {
+    this.isHansokuChuiClicked = !this.isHansokuChuiClicked;
   }
 
-  addChui2(participant: number) {
-    this.applyChui2(participant);
+  toggleHansoku() {
+    this.isHansokuClicked = !this.isHansokuClicked;
   }
 
-  addChui3(participant: number) {
-    this.applyChui3(participant);
-  }
-
-  addHansokuChui(participant: number) {
-    this.applyHansokuChui(participant);
-  }
-
-  applyChui1(participant: number) {
-    if (participant === 1) {
-      this.participant1Chui1Applied = true;
-    } else if (participant === 2) {
-      this.participant2Chui1Applied = true;
-    }
-    this.incrementChuiCount(participant);
-  }
-
-  applyChui2(participant: number) {
-    if (participant === 1) {
-      this.participant1Chui2Applied = true;
-    } else if (participant === 2) {
-      this.participant2Chui2Applied = true;
-    }
-    this.incrementChuiCount(participant);
-  }
-
-  applyChui3(participant: number) {
-    if (participant === 1) {
-      this.participant1Chui3Applied = true;
-    } else if (participant === 2) {
-      this.participant2Chui3Applied = true;
-    }
-    this.incrementChuiCount(participant);
-  }
-
-  applyHansokuChui(participant: number) {
-    if (participant === 1) {
-      this.participant1HansokuChuiApplied = true;
-    } else if (participant === 2) {
-      this.participant2HansokuChuiApplied = true;
-    }
-    this.incrementChuiCount(participant);
-  }
-
-  incrementChuiCount(participant: number) {
-    if (participant === 1) {
-      this.participant1Penalties++;
-    } else if (participant === 2) {
-      this.participant2Penalties++;
-    }
-  }
-
-  addHansoku(participantNumber: number) {
-
-  }
 }
