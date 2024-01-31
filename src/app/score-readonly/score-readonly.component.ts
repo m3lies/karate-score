bb69ae50e27cfed37282af201e9673d024bf5988
m3lies <melanie.manivanh@gmail.com>
2024-02-01 00:57:10 +0100

    added shared service to try to communicate with one another

--- src/app/score-readonly/score-readonly.component.ts
+++ src/app/score-readonly/score-readonly.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, OnDestroy, ChangeDetectorRef, ChangeDetectionStrategy} from '@angular/core';
-import { SharedDataService } from '../shared-data.service';
-import { Subscription } from 'rxjs';
+import {SharedDataService} from '../shared-data.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-score-readonly',
@@ -8,34 +8,33 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./score-readonly.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScoreReadOnlyComponent implements OnInit, OnDestroy {
+export class ScoreReadOnlyComponent implements OnInit {
   totalScores: number[] = [0, 0];
   scores: { yuko: number; wazaAri: number; ippon: number }[] = [
-    { yuko: 0, wazaAri: 0, ippon: 0 }, // Participant 1 scores
-    { yuko: 0, wazaAri: 0, ippon: 0 }, // Participant 2 scores
+    {yuko: 0, wazaAri: 0, ippon: 0}, // Participant 1 scores
+    {yuko: 0, wazaAri: 0, ippon: 0}, // Participant 2 scores
   ];
 
   private totalScoresSubscription: Subscription | undefined;
   private scoresSubscription: Subscription | undefined;
 
-  constructor(private sharedDataService: SharedDataService,     private changeDetectorRef: ChangeDetectorRef) {}
+  constructor(private sharedDataService: SharedDataService, private changeDetectorRef: ChangeDetectorRef) {
+  }
 
   ngOnInit() {
     // Subscribe to the observables to get the values from SharedDataService
     this.totalScoresSubscription = this.sharedDataService.totalScores$.subscribe((totalScores) => {
+      this.changeDetectorRef.detectChanges()
       this.totalScores = totalScores;
       console.log('Total Scores updated in readonly:', totalScores);
     });
 
     this.scoresSubscription = this.sharedDataService.scores$.subscribe((scores) => {
+      this.changeDetectorRef.detectChanges()
       this.scores = scores;
       console.log('Scores updated in readonly:', scores);
     });
   }
-  ngOnDestroy() {
-    // Unsubscribe to prevent further updates
-    this.totalScoresSubscription?.unsubscribe();
-    this.scoresSubscription?.unsubscribe();
-  }
+
 
 }
